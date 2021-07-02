ee13dac4e8d802908cacbdbccebec314c4ce5468
024William <william.m.tuominiemi@gmail.com>
2021-07-02 13:28:57 +0300

    made server express

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-var http = require('http');
+const express = require('express')
 var fs = require('fs');
 var path = require('path');
 const { exec } = require('child_process');
 
-http.createServer(function (req, res) {
-    console.log('request received')
-    console.log(`raspistill -o ${path.resolve(__dirname, 'image.jpg')}`)
-    exec(`raspistill -o ${path.resolve(__dirname, 'image.jpg')}`, (err, stdout, stderr) => {
-        if (err) {
-          // node couldn't execute the command
-          return;
-        }
+const app = express()
 
-        res.writeHead(200, {"Content-Type": "image/jpg"});
+app.get('/', function (req, res) {
+  console.log('request received')
+  console.log(`raspistill -o ${path.resolve(__dirname, 'image.jpg')}`)
+  exec(`raspistill -o ${path.resolve(__dirname, 'image.jpg')}`, (err, stdout, stderr) => {
+    if (err) {
+      // node couldn't execute the command
+      return;
+    }
 
-        fs.createReadStream(path.resolve(__dirname, 'image.jpg')) 
-        .pipe(res);
-    });
-}).listen(process.env.PORT || '3000'); // provide a default  
+    res.writeHead(200, { "Content-Type": "image/jpg" });
+
+    fs.createReadStream(path.resolve(__dirname, 'image.jpg'))
+      .pipe(res);
+  });
+})
+
+app.listen(3000)
