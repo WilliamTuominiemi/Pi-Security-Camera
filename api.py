b66a0a9c5fa68b2fa98e74949e69a8affe020046
024William <william.m.tuominiemi@gmail.com>
2021-07-04 16:17:37 +0300

    Servo is controlled by buttons on website

--- api.py
+++ api.py
@@ -1,7 +1,27 @@
 import flask
+import RPi.GPIO as GPIO
+import time
 
+# Setup app
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+# Setup servo
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(11, GPIO.OUT)
+servo1 = GPIO.PWM(11, 50)  # Pin 11, 50Hz
+
+servo1.start(0)
+
+
+def SetAngle(angle):
+    duty = angle / 18 + 2
+    GPIO.output(11, True)
+    servo1.ChangeDutyCycle(duty)
+    time.sleep(1)
+    GPIO.output(11, False)
+    servo1.ChangeDutyCycle(0)
 
 
 @app.route('/', methods=['GET'])
@@ -12,12 +32,16 @@ def home():
 @app.route('/right', methods=['GET'])
 def right():
     print('right')
+    SetAngle(30)
+    # GPIO.cleanup()
     return "<h1>RIGHT</p>"
 
 
 @app.route('/left', methods=['GET'])
 def left():
     print('left')
+    SetAngle(130)
+    # GPIO.cleanup()
     return "<h1>LEFT</p>"
 
 
