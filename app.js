9fe6cf5cd356b3d0c732cd44c214f6ec9531d904
024William <william.m.tuominiemi@gmail.com>
2021-07-04 22:10:22 +0300

    fighting with gitignore

--- app.js
+++ app.js
@@ -10,31 +10,12 @@ app.use('/static', express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  // const options = {
-  //   hostname: 'http://127.0.0.1:5000/',
-  //   method: 'GET'
-  // }
-
-  // const _req = https.request(options, _res => {
-  //   console.log(`statusCode: ${_res.statusCode}`)
-
-  //   _res.on('data', d => {
-  //     process.stdout.write(d)
-  //   })
-  // })
-
-  // _req.on('error', error => {
-  //   console.error(error)
-  // })
-
-  // _req.end()
-
   res.render('index.ejs');
 })
 
 app.get('/image', (req, res) => {
   console.log('request received')
-  exec(`raspistill -o ${path.resolve(__dirname, 'image.jpg')}`, (err, stdout, stderr) => {
+  exec(`raspistill -o ${path.resolve(__dirname, 'images/image.jpg')}`, (err, stdout, stderr) => {
     if (err) {
       console.log("node couldn't execute the command")
       return;
@@ -44,7 +25,7 @@ app.get('/image', (req, res) => {
 
     res.writeHead(200, { "Content-Type": "image/jpg" });
 
-    fs.createReadStream(path.resolve(__dirname, 'image.jpg'))
+    fs.createReadStream(path.resolve(__dirname, 'images/image.jpg'))
       .pipe(res);
   });
 })
